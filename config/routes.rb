9f627e47ec7968327ff14cbcc870255439064394
Kevin Cogen <k.cogen@gmail.com>
2023-07-21 17:35:30 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  resource :about, only: [:show], controller: "about"
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -15,85 +17,16 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
-  GifVault::Application.routes.draw do
-    
-    # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-    root to: 'gif#cool'
-    
-    # I've created a gif controller so I have a page I can secure later. 
-    # This is optional (as is the root to: above).
-    get '/cool' => 'gif#cool'
-    get '/sweet' => 'gif#sweet'
+  # Login form, logging in and logging out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
-    # These routes will be for signup. The first renders a form in the browse, the second will 
-    # receive the form and create a user in our database using the data given to us by the user.
+  # Signup form, and creating a new user
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
-      # Login form, logging in and logging out
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
-    
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
-
-end
-
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
